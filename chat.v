27f98adb24bb0feaf40712240bbb9d4cb95669ef
GG2018 <36273864+Redstoyn33@users.noreply.github.com>
2022-10-24 21:26:51 +0300

    make chat

--- chat.v
+++ chat.v
@@ -1,17 +1,18 @@
 import term.ui as tui
-import os
-import net
 import time
+import os
 
 struct Chat {
 mut:
-	tui    &tui.Context = unsafe { 0 }
-	name   string
-	format string
-	text   string
-	buf    LoopArr
-	inp    chan Com
-	out    chan Com
+	tui       &tui.Context = unsafe { 0 }
+	name      string
+	format    string
+	text      string
+	buf       &LoopArr
+	inp       chan Com
+	out       chan Com
+	level     u8
+	wait_scan i64
 }
 
 fn (mut c Chat) start() {
@@ -22,7 +23,10 @@ fn (mut c Chat) start() {
 		hide_cursor: false
 		frame_rate: 10
 	)
-	app.tui.run()?
+	c.tui.run() or {
+		os.input('Ошибка терминала')
+		exit(3)
+	}
 }
 
 fn event(e &tui.Event, x voidptr) {
@@ -33,21 +37,45 @@ fn event(e &tui.Event, x voidptr) {
 				exit(0)
 			}
 			.backspace {
-				if app.enter.len == 0 {
+				if c.text.len == 0 {
 					return
 				}
-				app.enter = app.enter[..app.enter.len - 1]
+				c.text = c.text[..c.text.len - 1]
 			}
 			.enter {
-				if app.enter.len == 0 {
+				if c.text.len == 0 {
 					return
 				}
-				app.out.try_push(app.enter)
-				app.buf << '< ' + app.enter
-				app.enter = ''
+				match c.text {
+					'/help' {
+						c.buf.add('Гуманитарная помощь:')
+					}
+					'/level' {
+						c.buf.add('Уровень $c.level')
+					}
+					'/scan' {
+						d := Scan{time.now().unix_time()}
+						c.out.try_push(d)
+						c.buf.add('Начало скана')
+					}
+					else {
+						if c.text.starts_with('/format') {
+							if c.text == '/format' {
+								c.buf.add('%n - имя, %m - сообщение')
+							} else {
+								c.format = c.text[8..]
+							}
+						} else {
+							d := Msg{c.name, c.text}
+							c.out.try_push(d)
+							c.buf.add(c.format.replace_each(['%n', c.name, '%m', c.text]))
+						}
+					}
+				}
+				c.text = ''
 			}
 			else {
-				app.enter += e.utf8
+				c.text += e.utf8
 			}
 		}
 	}
@@ -55,6 +83,40 @@ fn event(e &tui.Event, x voidptr) {
 
 fn frame(x voidptr) {
 	mut c := &Chat(x)
+	for {
+		select {
+			s := <- c.inp {
+				match s {
+					Msg {
+						c.buf.add(c.format.replace_each(['%n', s.name, '%m', s.msg]))
+					}
+					Resp {
+						s.info.print(mut c.buf, 0)
+					}
+					else{}
+				}
+			}
+			else {
+				break
+			}
+		}
+	}
+	c.tui.clear()
+	for i in 0 .. c.tui.window_height {
+		if c.buf.len <= i {
+			break
+		}
+		c.tui.draw_text(0, c.tui.window_height - 1 - i, c.buf.get(c.buf.len - 1 - i))
+	}
+	c.tui.draw_text(0, c.tui.window_height, c.enter )
+	c.tui.flush()
+}
+
+fn (i Info) print(mut a LoopArr, tab int) {
+	a.add('|-'.repeat(tab) + '$i.name [${i.level.str()}]')
+	for ii in i.sub {
+		ii.print(mut a, tab + 1)
+	}
 }
 
 struct LoopArr {
@@ -71,29 +133,4 @@ fn (a LoopArr) get(i int) string {
 fn (mut a LoopArr) add(s string) {
 	a.pos = if a.pos == 0 { a.len - 1 } else { a.pos - 1 }
 	a.arr[a.pos] = s
-}
-
-type Com = Msg | Resp | Scan
-
-struct Msg {
-	name string
-	msg  string
-}
-
-struct Scan {
-	time int
-}
-
-struct Resp {
-	info Info
-}
-
-struct Info {
-	level u8
-	name  string
-	sub   []Info
-}
-
-fn main() {
-	os
 }
