1d668138bbbf2aa224b50180d3add1e1a2b47661
GG2018 <36273864+Redstoyn33@users.noreply.github.com>
2022-10-24 21:27:08 +0300

    make start

---
+++ start.v
@@ -0,0 +1,67 @@
+import os
+import net as nett { TcpConn }
+
+fn main() {
+	name := os.input('Введите имя - ')
+	addr := os.input('Введите адрес (оставте пустым для хоста) - ')
+	mconn := if addr == '' {
+		&TcpConn(unsafe { 0 })
+	} else {
+		nett.dial_tcp(addr + ':65555') or {
+			os.input('Не удалось подключится')
+			exit(1)
+		}
+	}
+	level := if addr == '' {
+		0
+	} else {
+		mut buf := []u8{len: 1}
+		mconn.wait_for_read() or {
+			os.input('Ошибка соединения')
+			exit(2)
+		}
+		mconn.read(mut buf) or {
+			os.input('Ошибка соединения')
+			exit(2)
+		}
+		buf[0]
+	}
+
+	new_conn := chan &TcpConn{cap: 1}
+	out := chan Com{cap: 10}
+	inp := chan Com{cap: 10}
+
+	mut net := &Net{
+		level: level
+		mconn: mconn
+		conns: []&TcpConn{}
+		new_conn: new_conn
+		inp: inp
+		out: out
+		name: name
+	}
+	mut chat := Chat{
+		name: name
+		format: '%n > %m'
+		text: ''
+		buf: &LoopArr{
+			len: 10
+			arr: []string{cap: 10}
+		}
+		inp: inp
+		out: out
+		level: level
+		wait_scan: 120
+	}
+	mut accept := &Accept{
+		new_conn: new_conn
+		l: nett.listen_tcp(.ip, '65555') or {
+			os.input('Ошибка хостинга')
+			exit(4)
+		}
+		level: level
+	}
+	go net.start()
+	go accept.start()
+	chat.start()
+}
